7002001b1494c6311bd0dbf3dfea67dff5361542
Teo123k <s864bysd8y@privaterelay.appleid.com>
2026-02-04 05:43:34 +0300

    Update form submission email address

--- src/app/components/ContactForm/index.tsx
+++ src/app/components/ContactForm/index.tsx
@@ -38,7 +38,7 @@ const ContactForm = () => {
     e.preventDefault()
     setLoader(true)
 
-    fetch('https://formsubmit.co/ajax/bhainirav772@gmail.com', {
+    fetch('https://formsubmit.co/ajax/contact@cabij.co', {
       method: 'POST',
       headers: { 'Content-type': 'application/json' },
       body: JSON.stringify({
